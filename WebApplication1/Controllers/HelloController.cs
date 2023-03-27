61d40e19b757e54da03428171bde562892aa3722
Нікіта Драчевський <ki212_dnv@student.ztu.edu.ua>
2023-03-27 22:46:33 +0300

    init

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HelloController: Controller
+{
+    public IActionResult Index()
+    {
+        return View("HelloForm");
+    }
+
+    public IActionResult Hello(User user)
+    {
+        return View("HelloUser", user);
+    }
+}
